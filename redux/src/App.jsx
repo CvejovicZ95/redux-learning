4b906f225575ae11e7a87333496eb10a2d2cd0f1
CvejovicZ95 <cvejovicz@gmail.com>
2024-10-03 13:01:36 +0200

    redux basics 01 lesson finished

--- redux/src/App.jsx
+++ redux/src/App.jsx
@@ -1,8 +1,11 @@
+import { Counter } from "./features/counter/Counter";
+
 function App() {
   return (
     <div className="App">
       <h1>Redux</h1>
       <h2>01:Redux-basics</h2>
+      <Counter/>
     </div>
   );
 }
