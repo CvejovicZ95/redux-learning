e7db7fc5d4ed2ad9d9f6ead653878bbff07c7df0
CvejovicZ95 <cvejovicz@gmail.com>
2024-10-15 14:37:55 +0200

    added timestamp to front for posts

--- client/src/features/posts/PostsList.jsx
+++ client/src/features/posts/PostsList.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchPosts, selectAllPosts } from './postsSlice';
 import { PostAuthor } from './PostAuthor';
+import { TimeAgo } from "./TimeAgo"
 
 export const PostsList = () => {
     const dispatch = useDispatch();
@@ -11,11 +12,14 @@ export const PostsList = () => {
         dispatch(fetchPosts());
     }, [dispatch]);
 
-    const renderedPosts = posts.map((post) => (
+    const sortedPosts = posts.slice().sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+
+    const renderedPosts = sortedPosts.map((post) => (
         <article key={post._id}>
             <h2>{post.title}</h2>
             <p>{post.content}</p>
             <PostAuthor userId={post.userId}/>
+            <TimeAgo timestamp={post.created_at}/>
         </article>
     ));
 
