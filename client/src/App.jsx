8539934400034e633e650882d7789e332b3ec124
CvejovicZ95 <cvejovicz@gmail.com>
2024-10-21 12:18:47 +0200

    added users and their posts filter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
-//import { Counter } from "./features/counter/Counter";
 import { PostsList } from "./features/posts/PostsList";
 import { AddPostForm } from "./features/posts/AddPostForm";
 import { SinglePostPage } from "./features/posts/SinglePostPage";
 import { EditPostForm } from "./features/posts/EditPostForm";
+import { UsersList } from "./features/users/UsersList";
+import { UserPage } from "./features/users/UserPage";
 import { Layout } from "./components/Layout";
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 
 function App() {
   return (
@@ -18,6 +19,12 @@ function App() {
       <Route path="post/:postId" element={<SinglePostPage />} />
 
       <Route path="post/edit/:postId" element={<EditPostForm/>}/>
+
+      <Route path="user" element={<UsersList/>}/>
+
+      <Route path="user/:userId" element={<UserPage/>}/>
+
+      <Route path="*" element={<Navigate to={'/'} replace />} />
       
     </Route>
   </Routes>
---
+++ client/src/features/users/UserPage.jsx
@@ -0,0 +1,28 @@
+import { useSelector } from "react-redux";
+import { selectUserById } from "../users/usersSlice";
+import { selectAllPosts } from "../posts/postsSlice";
+import { Link, useParams } from "react-router-dom";
+
+export const UserPage = () => {
+    const { userId } = useParams()
+
+    const user = useSelector(state => selectUserById(state, userId))
+
+    const postsForUser = useSelector(state => {
+        const allPosts = selectAllPosts(state)
+        return allPosts.filter(post => post.userId._id === userId)
+    })
+
+    const postTitles = postsForUser.map(post => (
+        <li key={post._id}>
+            <Link to={`/post/${post._id}`}>{post.title}</Link>
+        </li>
+    ))
+
+    return (
+        <section>
+            <h2>{user?.username}</h2>
+            <ol>{postTitles}</ol>
+        </section>
+    )
+}
